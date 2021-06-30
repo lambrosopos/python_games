c2339171bf636270a4680607cb8f59a1fa68dd7e
lambrosopos <lambrosopos@gmail.com>
2021-06-30 23:46:31 +0900

    updating gitignore and game.py file

--- menu_experiment/game.py
+++ menu_experiment/game.py
@@ -21,6 +21,8 @@ class Game():
                 self.playing = False
 
             self.display.fill(self.BLACK)
+            self.draw_text('Thanks for Playing', 20, self.DISPLAY_W/2,
+                           self.DISPLAY_H/2)
             self.window.blit(self.display, (0, 0))
             pygame.display.update()
             self.reset_keys()
@@ -41,4 +43,12 @@ class Game():
 
     def reset_keys(self):
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+    def draw_text(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, self.WHITE)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x, y)
+        self.display.blit(text_surface, text_rect)
+
 
